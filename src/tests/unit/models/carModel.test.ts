a2f32ffc02aef66e14987eb14769eb069cee5ade
Murilo Rainho <murilorainho01@gmail.com>
2022-04-27 08:20:23 -0300

    test: ensure CarModel.read return all cars if all goes well

--- src/tests/unit/models/carModel.test.ts
+++ src/tests/unit/models/carModel.test.ts
@@ -37,8 +37,6 @@ describe('CarModel create method', () => {
     sinon.stub(mongooseModel, 'create').resolves({ ...validCar, _id: newId });
 
     const createdCar = await carModel.create(validCar);
-
-    console.log(createdCar);
     
     expect(createdCar).to.have.property('model');
     expect(createdCar).to.have.property('year');
@@ -47,6 +45,26 @@ describe('CarModel create method', () => {
     expect(createdCar).to.have.property('seatsQty');
     expect(createdCar).to.have.property('doorsQty');
     expect(createdCar).to.have.property('_id');
+  });
+
+});
+
+describe('CarModel read method', () => {
+
+  it('Should return all cars if all goes well', async () => {
+    const { carModel, mongooseModel } = factories();
+
+    sinon.stub(mongooseModel, 'find').resolves([{ ...validCar, _id: newId }] as any[]);
+
+    const [car] = await carModel.read();
+    
+    expect(car).to.have.property('model');
+    expect(car).to.have.property('year');
+    expect(car).to.have.property('color');
+    expect(car).to.have.property('buyValue');
+    expect(car).to.have.property('seatsQty');
+    expect(car).to.have.property('doorsQty');
+    expect(car).to.have.property('_id');
   });
 
 });
