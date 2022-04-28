fedc90bcb9ec41cbb2ff2d6f2b08d536162970e9
Murilo Rainho <murilorainho01@gmail.com>
2022-04-28 07:24:00 -0300

    test: create a valid motorcycle mock

--- src/tests/utils/mocks/index.ts
+++ src/tests/utils/mocks/index.ts
@@ -1,1 +1,2 @@
 export * from './validCar';
+export * from './validMotorcycle';
---
+++ src/tests/utils/mocks/validMotorcycle.ts
@@ -0,0 +1,10 @@
+import { Motorcycle } from '../../../interfaces';
+
+export const validMotorcycle: Motorcycle = {
+  model: 'Honda CG Titan 125',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500,
+  category: 'Street',
+  engineCapacity: 125
+};
