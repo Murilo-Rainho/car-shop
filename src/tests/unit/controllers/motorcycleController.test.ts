afc990283f19718bae320fed35108b3241024ccb
Murilo Rainho <murilorainho01@gmail.com>
2022-04-29 07:37:59 -0300

    test: ensure MotorcycleController.create return statusCode 201 and the created motorcycle when a valid request is made correctly

---
+++ src/tests/unit/controllers/motorcycleController.test.ts
@@ -0,0 +1,75 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import * as sinon from 'sinon';
+import mongoose from 'mongoose';
+
+import { MotorcycleController } from '../../../controllers';
+import { Motorcycle, MotorcycleResponse, ErrorResponse, HttpResponse, Service } from '../../../interfaces';
+import { validMotorcycle } from '../../utils/mocks';
+import { NextFunction, Request, Response } from 'express';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const throwError = new Error('something went wrong');
+const newId = new mongoose.Types.ObjectId();
+
+class MotorcycleServiceStub implements Service<Motorcycle, MotorcycleResponse> {
+  async create(_body: Motorcycle): Promise<HttpResponse<MotorcycleResponse | ErrorResponse>> {
+    return { statusCode: 201, body: { ...validMotorcycle, _id: newId } };
+  }
+
+  async read(): Promise<HttpResponse<MotorcycleResponse[]>> {
+    return { statusCode: 200, body: [{ ...validMotorcycle, _id: newId }] };
+  }
+
+  async readOne(_id: string): Promise<HttpResponse<MotorcycleResponse | ErrorResponse>> {
+    return { statusCode: 200, body: { ...validMotorcycle, _id: newId } };
+  }
+
+  async update(body: Motorcycle, _id: string): Promise<HttpResponse<MotorcycleResponse | ErrorResponse>> {
+    return { statusCode: 200, body: { ...body, _id: newId } };
+  }
+
+  async delete(_id: string): Promise<HttpResponse<MotorcycleResponse | ErrorResponse>> {
+    return { statusCode: 200, body: { ...validMotorcycle, _id: newId } };
+  }
+}
+
+interface FactoriesTypes {
+  motorcycleServiceStub: MotorcycleServiceStub;
+  motorcycleController: MotorcycleController;
+}
+
+const factories = (): FactoriesTypes => {
+  const motorcycleServiceStub = new MotorcycleServiceStub();
+  const motorcycleController = new MotorcycleController(motorcycleServiceStub);
+  return {
+    motorcycleServiceStub,
+    motorcycleController,
+  };
+};
+
+describe('MotorcycleController create method', () => {
+
+  const mockReq = {} as Request;
+  const mockRes = {} as Response;
+  let mockNext: NextFunction = () => {};
+
+  beforeEach(() => {
+    mockRes.status = sinon.stub().returns(mockRes);
+    mockRes.json = sinon.stub().returns({});
+    mockNext = sinon.stub().returns({});
+  });
+
+  it('Should return statusCode 201 and created car', async () => {
+    mockReq.body = validMotorcycle;
+    const { motorcycleController } = factories();
+
+    await motorcycleController.create(mockReq, mockRes, mockNext);
+    expect((mockRes.status as sinon.SinonStub).calledWith(201)).to.be.true;
+    expect((mockRes.json as sinon.SinonStub).calledWith({ ...validMotorcycle, _id: newId })).to.be.true;
+  });
+
+});
