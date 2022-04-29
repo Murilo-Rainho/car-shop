37d340410154dc56488c05d9c6a6f16a27e77180
Murilo Rainho <murilorainho01@gmail.com>
2022-04-28 21:16:21 -0300

    test: ensure MotorcycleService.readOne return statusCode 200 and the motorcycle to a valid request

--- src/tests/unit/services/motorcycleService.test.ts
+++ src/tests/unit/services/motorcycleService.test.ts
@@ -147,4 +147,16 @@ describe('MotorcycleService readOne method', () => {
     expect(httpResponse).to.be.eql(errors.notFoundResponse);
   });
 
+  it('Should return statusCode 200 and body with a motorcycle', async () => {
+    const validId = '62644a7a0ae3be566e672f14';
+    const { motorcycleService } = factories();
+
+    const httpResponse = await motorcycleService.readOne(validId);
+    
+    expect(httpResponse).to.have.property('statusCode');
+    expect(httpResponse).to.have.property('body');
+    expect(httpResponse.statusCode).to.be.eql(200);
+    expect(httpResponse.body).to.be.eql({ ...validMotorcycle, _id: newId });
+  });
+
 });
